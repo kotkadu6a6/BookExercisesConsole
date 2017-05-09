f8be0da87f0b01a73dfafee57e283a02738ab9a8
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-09 08:37:31 +0300

    Ex.1

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input \"r\"");
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(r, 2);
+            double circumference = 2 * Math.PI * r;
+            Console.WriteLine("The are of your circle is"+area);
+            Console.WriteLine("The circumference of your circle is"+circumference);
+            Console.ReadLine();
+
+        }
+    }
+}
