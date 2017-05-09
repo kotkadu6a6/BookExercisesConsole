173461d1d2e4605c4d3fda808913245898832bc4
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-09 09:37:45 +0300

    Ex.4
    Added Exercise 4

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a positive fraction number");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a negative fraction");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0,-10:X}{1,-10:0.00}{2,-10:0.00}",a,b,c);
+            Console.ReadLine();
+
+        }
+    }
+}
