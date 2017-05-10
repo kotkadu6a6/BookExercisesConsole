249d994b041075fefd09c933ba3ceb08bd80cbfa
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-10 09:46:50 +0300

    Ex.6
    Added the sixth exercise

---
+++ Ex6/Ex6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number");
+            int b = int.Parse(Console.ReadLine());
+            int max = Math.Max(a, b);
+            Console.WriteLine(max);
+            Console.ReadLine();
+
+        }
+    }
+}
