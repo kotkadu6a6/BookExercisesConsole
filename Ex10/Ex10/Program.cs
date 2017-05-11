bc5641d1aeb6c4e461304d218303089ba00faa11
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-11 21:59:13 +0300

    Ex.10
    Added Exercise 10 from the book

---
+++ Ex10/Ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the amount of numbers");
+            int input = int.Parse(Console.ReadLine());
+            uint number = new int();
+            long sum = new int();
+            for (int i = 1; i <= input; i++)
+            {
+                Console.WriteLine("Input a number to sum:");
+                number = uint.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine("The sum of the numbers is: "+sum);
+            Console.ReadLine();
+        }
+    }
+}
