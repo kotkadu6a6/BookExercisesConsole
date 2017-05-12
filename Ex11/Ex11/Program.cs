aa35d5192dd0eb6a4b83338327729678b941ce40
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-12 08:14:00 +0300

    Ex.11
    Adde the eleventh exetcise from the book

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number ");
+            int userNumber =int.Parse( Console.ReadLine());
+            for (int i = 1; i <= userNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
