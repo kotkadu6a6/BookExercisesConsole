9f318e980de12a9f2652eadff93a4a3b5684c38a
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-12 08:49:12 +0300

    Ex12
    Added the 12th exercise

---
+++ Ex12/Ex12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int n = int.Parse(Console.ReadLine());
+                long lastNumber = 0;
+                long newNumber = 1;
+                long beforeLastNumber;
+                if (n == 0)
+                {
+                }
+                else if (n == 1)
+                {
+                    Console.WriteLine(lastNumber);
+                }
+                else if (n == 2)
+                {
+                    Console.WriteLine("{0}, {1}", lastNumber, newNumber);
+                }
+                else
+                {
+                    Console.Write("{0}, {1}, ", lastNumber, newNumber);
+
+                    for (int i = 2; i < n; i++)
+                    {
+                        beforeLastNumber = lastNumber;
+                        lastNumber = newNumber;
+                        newNumber = beforeLastNumber + lastNumber;
+                        if (i < n - 1)
+                        {
+                            Console.WriteLine("Number{0}{1}, ",i, newNumber);
+                        }
+                        else
+                        {
+                            Console.WriteLine(newNumber);
+                        }
+
+
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
